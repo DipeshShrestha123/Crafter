a4982f4440449667a7545600999a33c38b7f0df7
Dipesh shrestha <110885262+DipeshShrestha123@users.noreply.github.com>
2023-01-29 12:54:08 +0530

    Update signup.js

--- signup.js
+++ signup.js
@@ -89,8 +89,6 @@ if(eyeslash.classList.contains("fa-eye")){
 else{
 
 loginId.addEventListener("click", (e)=> {
-
-  alert("button is clicked");
   var loginEmail = document.getElementById("loginEmail").value;
   var loginPassword = document.getElementById("loginPassword").value;
   
@@ -124,43 +122,3 @@ loginId.addEventListener("click", (e)=> {
 }
 
 
-
-
-// const usernames = ["david", "david1", "david2","dipesh"];
-
-
-//   alert = document.getElementById("alert");
-
-// const updateUi = (value) => {
-//   console.log("value", value);
-//   spinner.classList.remove("visible");
-
-//   const usernameExists = usernames.some((u) => u === value);
-
-//   console.log("usernames", usernames);
-//   console.log("usernameExists", usernameExists);
-
-//   if (usernameExists) {
-//     alert.classList.add("visible");
-//   } else {
-//     alert.classList.remove("visible");
-//   }
-// };
-
-// const debounce = (callback, time) => {
-//   let interval;
-//   return (...args) => {
-//     clearTimeout(interval);
-//     interval = setTimeout(() => {
-//       callback.apply(null, args);
-//     }, time);
-//   };
-// };
-
-
-
-// const handleChange = debounce((input) => {
-//   const { value } = input.target;
-
-//   updateUi(value);
-// }, 500);
